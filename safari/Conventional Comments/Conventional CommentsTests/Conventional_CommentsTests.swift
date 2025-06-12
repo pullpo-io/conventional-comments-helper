12e02b12d3b2318ad1690dc27c087158483f8ea9
Rodrigo Moreno <moreno.rdr@gmail.com>
2025-06-11 20:17:52 -0600

    feature: add Safari support
    create from safari-web-extension-converter

---
+++ safari/Conventional Comments/Conventional CommentsTests/Conventional_CommentsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Conventional_CommentsTests.swift
+//  Conventional CommentsTests
+//
+//  Created by Rodrigo Moreno on 22/05/25.
+//
+
+import Testing
+@testable import Conventional_Comments
+
+struct Conventional_CommentsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
